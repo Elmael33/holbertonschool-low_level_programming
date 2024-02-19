dc0c03c3402929e597fd53951c4c09a064ce4ec3
Elmael33 <mael.salet33@gmail.com>
2024-02-19 14:55:56 +0000

    Task C n*2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,1 +1,14 @@
-gcc -o 4-puts_0 -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
+#include <stdio.h> 
+
+/*
+ * main - puts
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
