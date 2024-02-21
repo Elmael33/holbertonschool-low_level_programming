6b9a3c01e61880eb66cb8c9b222a931e06b5cea6
Elmael33 <mael.salet33@gmail.com>
2024-02-21 14:18:06 +0000

    P1 Task 1 v7

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,27 +2,30 @@
 #include <stdio.h>
 #include <time.h>
 /**
- * Program pour trouver le debut ou la fin
+ * I thought it was a doc line
  *
- * Return
+ * Return: 0
  */
-int main(void) {
-
+int main(void)
+/**
+ *
+ */
+{
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n%10);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else if (n < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n%10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-	else 
+	else
 	{
-		printf("Last digit of %d is %d and is 0\n", n, n%10);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	return (0);
 }
