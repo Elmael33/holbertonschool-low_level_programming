cf588ded306da290d2052b707d7662645e6b7150
Elmael33 <mael.salet33@gmail.com>
2024-02-22 14:15:37 +0000

    P1 Task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print reverse alphabet in lower
+ * Return: Always 0
+ */
+int main(void)
+{
+	char(reverse);
+
+	for (reverse = 'z'; reverse <= 'a'; reverse++)
+	{
+		putchar(reverse);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
