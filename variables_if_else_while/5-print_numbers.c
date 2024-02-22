1afcf07745857c0363607925b6b599b6eb9bd555
Elmael33 <mael.salet33@gmail.com>
2024-02-22 00:07:14 +0000

    P1 Task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print a snigle digit between one and ten
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++);
+		printf(numbers"%d");
+	
+	printf("\n");
+
+	return (0);
+}
