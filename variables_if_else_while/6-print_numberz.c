8313ad4d3283919ef1f77e4b190189c1e0da9ac2
Elmael33 <mael.salet33@gmail.com>
2024-02-22 13:40:53 +0000

    P1 Task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - printa single digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numberz;
+	
+	for (numberz = 0; numberz < 10; numberz++)
+			putchar(numberz);
+	
+	putchar('\n');
+	return (0);
+}
