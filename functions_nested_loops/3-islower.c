9748a7e4bbe5874ea5489e72547041a4bf1a3752
Elmael33 <mael.salet33@gmail.com>
2024-02-23 14:19:00 +0000

    P2 Task 3 v3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,22 +1,13 @@
 #include "main.h"
 #include <unistd.h>
-#include <islower>
 /**
  * _islower - checks for lowercase character
  * Return: Always 0
  */
-int _islower(int c);
+int _islower(int c)
 {
-	char check;
-
-	if (check = 'a'; check <= 'z'; check++)
-
-
-	else if (check = 'A'; check <= 'Z'; check++)
-
-
-	else (check !='1'; check <= '0')
-
-	return (0);
-
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
 }
