2baa4629e89dc7178d1b074d340ba9bc8cda5d11
Elmael33 <mael.salet33@gmail.com>
2024-02-23 13:11:38 +0000

    P2 Task3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+#include <islower>
+/**
+ * _islower - checks for lowercase character
+ * Return: Always 0
+ */
+int _islower(int c);
+{
+	char check;
+
+	if (check = 'a'; check <= 'z'; check++)
+
+
+	else if (check = 'A'; check <= 'Z'; check++)
+
+
+	else (check !='1'; check <= '0')
+
+	return (0);
+
+}
