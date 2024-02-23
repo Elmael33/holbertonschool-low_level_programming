737b286b6f68c9cb82d4cff3a32197ce04ff0801
Elmael33 <mael.salet33@gmail.com>
2024-02-23 08:28:37 +0000

    P2 Task2 fix2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main - print alphabet 10x
+ * print_alphabet_x10 - print alphabet 10x
  * Return: Always 0
  */
 void print_alphabet_x10(void)
 {
 	int g = 0;
 	char alphab;
+
 	while (g++ < 10)
-	{	
+	{
 		for (alphab = 'a'; alphab <= 'z'; alphab++)
 		{
 			_putchar(alphab);
