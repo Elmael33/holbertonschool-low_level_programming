8d18f7a38e6e7e3a18dc9772c7d89f5e2b02d56f
Elmael33 <mael.salet33@gmail.com>
2024-02-23 00:35:19 +0000

    P2 task1 v5

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main - prints the alphabet in lowercase
+ * print_alphabet - prints the alphabet in lowercase
  * Return: Always 0
  */
 void print_alphabet(void)
