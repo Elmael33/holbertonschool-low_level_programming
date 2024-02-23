de3eaa4a214e815d2f0cd4ddda25690811094acb
Elmael33 <mael.salet33@gmail.com>
2024-02-23 17:03:25 +0000

    P2 Task5 v1

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,8 +1,28 @@
 #include "main.h"
 #include <unistd.h>
 /**
- *
- *
+ * print_sign - print the sign of a number
+ * Return: Always 0
  */
-int print_sign(int n);
+int print_sign(int n)
+{
+	char numo;
+
+	if (numo > '0')
+	{
+		printf(+)
+		return (1);
+	}
+
+	else if (numo < '0')
+	{
+		printf(-)
+		return (-1);
+	}
+	else 
+	{
+		printf(0)
+		return (0);
+	}
+}
 
