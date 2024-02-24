23fbe3a58bd8eb78618732cca395c0bb22a69079
Elmael33 <mael.salet33@gmail.com>
2024-02-24 10:08:42 +0000

    P2 Task5 v2

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,24 +4,24 @@
  * print_sign - print the sign of a number
  * Return: Always 0
  */
-int print_sign(int n)
+int print_sign(int numo)
 {
 	char numo;
 
 	if (numo > '0')
 	{
-		printf(+)
+		putchar('+')
 		return (1);
 	}
 
 	else if (numo < '0')
 	{
-		printf(-)
+		putchar('-')
 		return (-1);
 	}
 	else 
 	{
-		printf(0)
+		putchar('0')
 		return (0);
 	}
 }
