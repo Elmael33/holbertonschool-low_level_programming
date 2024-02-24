9bcb1299045d22f57483490cca3055c22484d40f
Elmael33 <mael.salet33@gmail.com>
2024-02-24 15:05:11 +0000

    P2 Task5 encore

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,24 +3,26 @@
 /**
  * print_sign - print the sign of a number
  * @n: check the sign if a number is upper or lower
- * Return: Always 0
+ * Return: 1 if is upper, -1 if is lower and 0 if is zero
  */
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == '0')
+
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
+
 	else
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 }
 
