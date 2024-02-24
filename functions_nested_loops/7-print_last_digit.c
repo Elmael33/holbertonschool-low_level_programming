bdfb25f84281987db1b52dc6a283b1196533bcb6
Elmael33 <mael.salet33@gmail.com>
2024-02-24 16:20:32 +0000

    P2 Task7 V2~3

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,15 @@
 /**
  * print_last_digit - print the last digit of a number
  * @numo: random number who print last digit
- * Return: Always 0
+ * Return: Last_digit
  */
-int print_last_digit(int numo)
+int print_last_digit(int n)
 {
-	int last_digit = numo % 10;
+	int last_digit = n % 10;
 
 	if (last_digit < 0)
 	{
-		last_digit = -1;
+		(last_digit = -1);
 	}
 
 	_putchar(last_digit + '0');
