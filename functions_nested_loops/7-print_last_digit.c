561d574ed75bae80dd698975c6c7ef425d8a386a
Elmael33 <mael.salet33@gmail.com>
2024-02-24 19:20:22 +0000

    P2 Task7 v4

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,14 +7,14 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last = n % 10;
 
-	if (last_digit < '0')
+	if (last < 0)
 	{
-		(last_digit *= -1);
+		last *= -1;
 	}
 
-	_putchar(last_digit + '0');
+	_putchar(last + '0');
 
-	return (last_digit);
+	return (last);
 }
