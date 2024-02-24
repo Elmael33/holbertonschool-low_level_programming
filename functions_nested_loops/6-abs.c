eedd02a8b6200a7749cbe4a0e784aaee15fcb148
Elmael33 <mael.salet33@gmail.com>
2024-02-24 12:58:47 +0000

    P2 Task6 v1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _abs - compute the absolute value of n
+ * @n: absolute integer
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n <= '0')
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
