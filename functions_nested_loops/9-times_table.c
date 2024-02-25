4430fc4588e2bed833c874da0da7da4d9113b049
Elmael33 <mael.salet33@gmail.com>
2024-02-25 14:44:23 +0000

    P2 Task9 fix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,29 +12,28 @@ void times_table(void)
 
 	for (numbers = 0; numbers <= 9; numbers++)
 	{
-		_putchar('0');
+		for (symbols = 1; symbols <= 9; symbols++)
+		{
+			characters = numbers * symbols;
+
+			if (symbols == 0)
+			{
+				_putchar(characters + '0');
+			}
+			else if (characters < 9 && symbols != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(characters + '0')
+			}
+			else if (characters >= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((characters / 10) + '0');
+				_putchar((characters % 10) + '0'):
+			}
+		}
+		_putchar('\n');
 	}
-
-	for (symbols = 1; symbols <= 9; symbols++)
-	{
-		_putchar(',');
-		_putchar(' ');
-	}
-
-	characters = numbers * symbols;
-
-	if (characters <= 9)
-	{
-		_putchar(' ');
-	}
-	else
-	{
-		_putchar((characters / 10) + '0');
-	}
-
-	_putchar((characters % 10) + '0');
-
-	_putchar('\n');
 }
-
-
