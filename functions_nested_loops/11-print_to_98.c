cdbf70f7f154ce4fbf3cefcd75f1d0b57bf9c381
Elmael33 <mael.salet33@gmail.com>
2024-02-25 18:19:34 +0000

    P2 Task11 v1

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -5,4 +5,45 @@
  *
  *
  */
-void print_to_98(int n);
+void print_to_98(int n)
+{
+	int france,bresil;
+
+	if (n >= 98)
+	{
+		for (france = n; france <= 98; france++);
+		{
+			if (france != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(france / 10 + '0');
+				_putchar(france % 10 + '0');
+			}
+			else
+			{
+				_putchar(france / 10 + '0');
+				_putchar(france % 10 + '0');
+			}
+		}
+	}
+	else if (n <= 98)
+	{
+		for (bresil = n; bresil >= 98; bresil--)
+		{
+			if (bresil != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(bresil / 10 + '0');
+				_putchar(bresil % 10 + '0');
+			}
+			else
+			{
+				_putchar(bresil / 10 = '0');
+				_putchar(bresil / 10 = '0');
+			}
+		}
+	_putchar('\n');
+	}
+}
