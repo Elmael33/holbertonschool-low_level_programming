10359e8712d3958fadd88dbef2772d285e0978ef
Elmael33 <mael.salet33@gmail.com>
2024-02-25 13:07:16 +0000

    P2 Task9 v1

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,7 +1,30 @@
 #include "main.h"
-#include <unistd>
+#include <unistd.h>
 /**
  *
  *
  */
-void times_table(void);
+void times_table(void)
+{
+	int numbers, symbols, characters;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		_putchar('0');
+	}
+
+	for (symbols = 1; symbols <= 9; symbols++)
+	{
+		_putchar(',');
+		_putchar(' ');
+		
+		if (characters <= 9)
+			_putchar(' ');}
+		else
+			_putchar((characters / 10 + '0');
+
+		_putchar((characters % 10) + '0');
+	}
+	putchar('\n');
+}
+
