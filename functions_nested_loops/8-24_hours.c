397a1ccd6d8d60c64f7a45c58507dbce652badc4
Elmael33 <mael.salet33@gmail.com>
2024-02-25 12:11:39 +0000

    P2 Task8 v~Ã8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,9 +1,26 @@
 #include "main.h"
-#include <unistd>
+#include <unistd.h>
 /**
- *
- *
- *
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * @heures,minutes: hours and minutes
+ * Return: from 00:00 to 23:59
  */
 void jack_bauer(void)
+{
+	int heures, minutes;
 
+	for (heures = 0; heures <= 23; heures++)
+
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+
+		{
+			_putchar((heures / 10) + '0');
+			_putchar((heures % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
