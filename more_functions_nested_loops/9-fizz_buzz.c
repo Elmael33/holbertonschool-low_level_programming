b99984b91b3ce340bff0fc7b2e311940495b4be0
Elmael33 <mael.salet33@gmail.com>
2024-02-29 09:15:04 +0000

    Task9 v4

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,19 +8,19 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a <= 100; a ++)
+	for (a = 1; a <= 100; a++)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Buzz");
+			printf("Fizz");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("Buzz");
 		}
 		else
 		{
