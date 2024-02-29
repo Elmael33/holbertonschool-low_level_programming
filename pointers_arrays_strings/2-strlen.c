e337ef3d634639a567b35e8f5a7813ed2359aaf6
Elmael33 <mael.salet33@gmail.com>
2024-02-29 23:36:06 +0000

    Task2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: integer
+ * Return: 
+ */
+int _strlen(char *s)
+
+{
+	int a;
+
+	for (a = 0; s[a] !='\0'; a++)
+	{
+		s++;
+	}
+	
+	return (a);
+}
