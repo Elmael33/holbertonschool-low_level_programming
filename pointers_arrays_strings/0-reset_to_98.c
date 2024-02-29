93bcfd52a902e9e486ae93a99ef6b78d938d938c
Elmael33 <mael.salet33@gmail.com>
2024-02-29 23:08:50 +0000

    Task0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * @n: integer
+ * Return: 98
+ */
+void reset_to_98(int *n)
+{
+        *n = 98;
+}
