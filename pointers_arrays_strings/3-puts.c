a95beb90177b11a664864a3c78f4c9fa14aefefe
Elmael33 <mael.salet33@gmail.com>
2024-03-01 14:12:28 +0000

    Task3 v3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _puts -  prints a string, followed by a new line, to stdout.
  * @str: pointeur
@@ -9,7 +8,7 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		putchar(*str++);
+		_putchar(*str++);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
