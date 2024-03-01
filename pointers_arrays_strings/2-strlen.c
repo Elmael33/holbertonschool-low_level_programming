4f5fa3e10a0a9bc957b0364b01fae972a82b03c2
Elmael33 <mael.salet33@gmail.com>
2024-03-01 09:58:54 +0000

    Task2 v2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,16 @@
 /**
  * _strlen - returns the length of a string.
  * @s: integer
- * Return: 
+ * Return: 0
  */
 int _strlen(char *s)
 
 {
 	int a;
 
-	for (a = 0; s[a] !='\0'; a++)
+	for (a = 0; s[a] != '\0'; a++)
 	{
+
 	}
-	
 	return (a);
 }
