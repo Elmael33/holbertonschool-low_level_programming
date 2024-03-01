ca56d82e98e66d042b54a60e4d3fe014dd83e781
Elmael33 <mael.salet33@gmail.com>
2024-03-01 13:35:12 +0000

    Task3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 
- *
+ * _puts -  prints a string, followed by a new line, to stdout.
+ * @str: pointeur
+ * Return: oui
  */
 void _puts(char *str)
 {
