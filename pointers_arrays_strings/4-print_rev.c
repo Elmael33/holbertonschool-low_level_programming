5f1a826d4926f1c346f2bc5da7d0874d10dfbd03
Elmael33 <mael.salet33@gmail.com>
2024-03-02 12:30:02 +0000

    Task4 v2

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,15 @@
  */
 void print_rev(char *s)
 {
-	int a;
+	int a = 0;
 
-	for (a = 0; s[a] != '\0')
+	while (s[a] != '\0')
 	{
 		a++;
 	}
-	for (a = -1; a >= 0; a--)
+	for (a -= 1; a >= 0; a--)
 	{
-		_putchar s[a];
+		_putchar(s[a]);
 	}
 	_putchar('\n');
 
