6f022f64ca8f9999be7183da223b0580d4718628
Elmael33 <mael.salet33@gmail.com>
2024-03-03 18:31:02 +0000

    Task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: integer
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
+
+	while (dest[a])
+	{
+		a++;
+	}
+	
+	while (b < n && ((dest[a]) = (src[b])))
+	{
+		a++;
+		b++;
+	}
+	dest[a + n + 1] = '\0';
+
+	return (dest);
+}
