b7171941295262928052e8fee153e5a9c0754d5e
Elmael33 <mael.salet33@gmail.com>
2024-03-03 18:39:59 +0000

    save

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: integer
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
