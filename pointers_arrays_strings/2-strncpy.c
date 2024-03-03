4db38fa49ead7046a14b759877cd6fb1aa1aa4be
Elmael33 <mael.salet33@gmail.com>
2024-03-03 22:04:50 +0000

    Task3

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,5 +9,25 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int a = 0, b = 0;
 
+	while (src[b])
+	{
+		b++;
+	}
+	while (a < n)
+	{
+		if (a < b)
+		{
+			dest[a] = src[a];
+		}
+		else
+		{
+			dest[a] = '\0';
+		}
+		{
+			a++;
+		}
+	}
+	return (dest);
 }
