c5076e180d7867fa9a129ec4d373e79e61fe91ac
Elmael33 <mael.salet33@gmail.com>
2024-03-04 19:39:39 +0000

    Task3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strspn - length of a prefix substring
+ * @s: pointer
+ * @accept: string to include
+ * Return: initial pointer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a = 0, b;
+
+	while (s[a] != '\0')
+	{
+		if (s[a] == ' ')
+		{
+			break;
+		}
+		for (b = 0; accept[a] != '\0'; a++)
+		{
+			if (s[a] == accept[b])
+			{
+				break;
+			}
+			if (accept[b] == '\0')
+			{
+				break;
+			}
+			{
+				a++;
+			}
+		}
+	}
+	return (a);
+}
