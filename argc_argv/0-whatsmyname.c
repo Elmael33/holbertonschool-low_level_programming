ccbb64a9d46ceac680063b4160081a2985ad6fb4
Elmael33 <mael.salet33@gmail.com>
2024-03-08 16:30:30 +0000

    Task2

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: integer
+ * @argv: pointer
+ * Return: (always) succes
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
