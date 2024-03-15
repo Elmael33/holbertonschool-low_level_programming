0286def0a39944d91f1bad57fb2c2adf9629a87e
Elmael33 <mael.salet33@gmail.com>
2024-03-15 15:16:28 +0000

    Task4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _copy - copy of name and owner
+ * @c: pointer
+ * @d: pointer
+ */
+void _copy(char *c, char *d)
+{
+	int e;
+
+	for (e = 0; d[e]; e++)
+		c[e] = d[e];
+	c[e] = '\0';
+}
+
+/**
  * new_dog - creates a new dog
  * @name: char
  * @age: float
@@ -10,38 +24,36 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t len1;
+	dog_t *len1;
 
 	int a, b;
 
 	for (a = 0; a[name]; a++)
+		;
+	for (b = 0; b[owner]; b++)
+		;
+
+	len1 = malloc(sizeof(dog_t));
+
+	if (len1 == NULL)
 	{
-		printf("%d", a);
+		return (NULL);
 	}
 
-	for (b = 0; b[owner]; b++)
-	{
-		printf("%d", b);
-	}
-	{
-		len1 = malloc(sizeof (dog_t));
-	}
-	{
-		if (len1 == NULL)
-		{
-			return (NULL);
-		}
-	}
-	{
-		len1-> name = malloc(a + 1);
-		len1-> owner = malloc(b + 1);
-	}
-	if (dog_h = name = NULL && dog_h = owner = NULL)
+	len1->name = malloc(a + 1);
+	len1->owner = malloc(b + 1);
+
+	if (len1->name == NULL && len1->owner == NULL)
 	{
 		free(len1->name);
 		free(len1->owner);
 		free(len1);
 		return (NULL);
 	}
+
+	_copy(len1->name, name);
+	_copy(len1->owner, owner);
+	len1->age = age;
+
+	return (len1);
 }
-void _copy
