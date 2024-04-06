25c116256ec443e8593c578550719de4597d7cd6
Elmael33 <mael.salet33@gmail.com>
2024-04-06 17:47:23 +0200

    write a function that returns the sum of all the data in a double linked list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list.
+ * @head: head of the list
+ * Return: if the list empty return NULL else return the sum of all data.
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
