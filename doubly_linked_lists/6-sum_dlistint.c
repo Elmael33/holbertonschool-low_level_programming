392d01e7bde89852b01ed7045fcc032400defb2d
Elmael33 <mael.salet33@gmail.com>
2024-04-06 17:51:29 +0200

    kernel style code correction

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list.
+ * sum_dlistint - returns the sum of all the data of a dlistint_t linked list.
  * @head: head of the list
  * Return: if the list empty return NULL else return the sum of all data.
 */
